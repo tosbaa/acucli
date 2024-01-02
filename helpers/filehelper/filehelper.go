99b39f5261076b1fa52b7f14b2cf8183c925038f
yagiz kocer <yagizkocer@gmail.com>
2024-01-02 16:32:59 +0300

    Added helper package to use globally

---
+++ helpers/filehelper/filehelper.go
@@ -0,0 +1,54 @@
+package filehelper // Check if the input is a file path
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func IsFilePath(input string) (bool, string) {
+	// Check if the file exists in the current directory
+	if _, err := os.Stat(input); err == nil {
+		absPath, _ := filepath.Abs(input)
+		return true, absPath
+	}
+	return false, ""
+}
+
+// Read file contents and return as an array of strings
+func ReadFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var contents []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		contents = append(contents, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return contents, nil
+}
+
+func ReadStdin() []string {
+	var inputArray []string
+	file := os.Stdin
+	fi, err := file.Stat()
+	if err != nil {
+		fmt.Println("file.Stat()", err)
+	}
+	if fi.Size() > 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			inputArray = append(inputArray, scanner.Text())
+		}
+	}
+	return inputArray
+}
